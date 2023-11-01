1caab73307cff6a210162a83868876ac01124e5a
tjvxh <61920928+tjvxh@users.noreply.github.com>
2023-11-01 16:29:53 -0700

    Folders

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ScottsdaleSchoolScout
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
